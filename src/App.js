85caed310795d360c0b01f7f943e82c4675ef146
Daniel Baigel <danielbaigel@Daniels-MBP.fios-router.home>
2020-07-23 13:32:30 -0400

    created news scraping chrome extension

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+/* global chrome */
+
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [domain, setDomain] = useState("")
+  const [headlines, setHeadlines] = useState("")
+  
+  useEffect(() => {
+    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+      const url = new URL(tabs[0].url)
+      const domain = url.hostname
+      setDomain(domain)
+      getHeadlines(domain)
+    })
+  }, [])
+
+  const getHeadlines = query => {
+    console.log("query is: ", query)
+    /* api call to get news headlines */
+    
+    
+    /* TOP HEADLINES IN USA */
+    // fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=be845022d39043fda6be28c7bea613c4`) 
+    /* HEADLINES FOR SPECIFIC SITE */
+    fetch(`https://newsapi.org/v2/everything?q='${query}'&language=en&apiKey=be845022d39043fda6be28c7bea613c4`)
+    .then(response => response.json())
+    .then(results => {
+      setHeadlines(results.articles.slice(0,5))
+    })
+    .catch(err => console.log("could not fetch headlines: ", err))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1 className="App-title">{domain}</h1>
+        Top Headlines:
+        {headlines ? headlines.map(headline => (
+          <h4 className="link" onClick={() => {
+            window.open(headline.url)}}>{headline.title}</h4>)) :
+            null
+        }
     </div>
   );
 }
